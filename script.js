489f18a2d1dc7782e877f3af0a997ee2aa4d3016
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2019-09-29 18:02:34 -0400

    update date format in index.html file

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Working!');
+var today = new Date();
+
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+const day = today.toLocaleString('en-US', options)
+const date = document.getElementById('date').innerHTML = day;
+
+console.log();
+
+// dayOfTheWeek
+// month
+// dayOfTheMonth
+
+
+
+
+
