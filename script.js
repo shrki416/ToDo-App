1922f762d801699c6aa93df1bf5504738794c8ea
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2019-09-30 15:38:38 -0400

    continue to work on the todo list styling

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 var today = new Date();
-
 var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-const day = today.toLocaleString('en-US', options)
-const date = document.getElementById('date').innerHTML = day;
+const date = document.getElementById('date').innerHTML = today.toLocaleString('en-US', options)
+// const day = today.toLocaleString('en-US', options)
+// const date = document.getElementById('date').innerHTML = day;
 
-console.log();
 
-// dayOfTheWeek
-// month
-// dayOfTheMonth
+
 
 
 
