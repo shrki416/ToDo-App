c6f5f3c87ac6bc8130e7cb8c2dae980893dfb2db
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2019-10-04 10:09:24 -0400

    add & remove functionality complete

--- script.js
+++ script.js
@@ -1,37 +1,58 @@
-var today = new Date();
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+let today = new Date();
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 const date = document.getElementById('date').innerHTML = today.toLocaleString('en-US', options)
 // const day = today.toLocaleString('en-US', options)
 // const date = document.getElementById('date').innerHTML = day;
 
 // get value of the input
+let form = document.getElementById('addForm');
 
-var form = document.getElementById('addForm');
-var todos = document.getElementById('items');
+let todos = document.getElementById('items');
 
-// Submit form event
+// submit event
 form.addEventListener('submit', addItem);
+// delete event
+todos.addEventListener('click', removeItem);
+// complete event
 
-// Add Item
+
+// add item
 function addItem(e){
     e.preventDefault(); 
 
-    // get input value
-    var newTodo = document.getElementById('addTodo').value;
-    // create new element - list item
-    var newItem = document.createElement('li');
-    // add class name to li
+        // get input value
+    let newTodo = document.getElementById('addTodo').value;
+        // create new element - list item
+    let newItem = document.createElement('li');
+        // add class name to li
     newItem.className = 'todoList';
-    // add text node with input value
+        // add text node with input value
     newItem.appendChild(document.createTextNode(newTodo));
-    // create the delete button element
-    var deleteBtn = document.createElement('span');
-    // add class to delete button
-    deleteBtn.className = 'fas fa-trash-alt';
-    // add delete button to li element
+        // create the delete button element
+    let deleteBtn = document.createElement('span');
+        // add class to delete button
+    deleteBtn.className = 'fas fa-trash';
+        // add delete button to li element
     newItem.appendChild(deleteBtn);
-    // add newly created (user) li to list-items
+        // create check mark
+    let checkMark = document.createElement('span');
+        // add class to check mark
+    checkMark.className = 'fas fa-check';
+        // add check mark to li element
+    newItem.appendChild(checkMark);
+        // add newly created (user) li to list-items
     todos.appendChild(newItem);
-
 }
+
+// remove item
+
+function removeItem(e){
+    if(e.target.classList.contains('fa-trash')){
+        let li = e.target.parentElement;
+        todos.removeChild(li);
+    }
+}
+
+// complete item
+
