70de9c42682276fb122090fffa67c00e1b60e651
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2019-10-09 21:44:55 -0400

    begin refactoring

--- script.js
+++ script.js
@@ -1,51 +1,53 @@
-let today = new Date();
-let options = {
+const todaysDate = new Date();
+const options = {
   weekday: "long",
   year: "numeric",
   month: "long",
   day: "numeric"
 };
 
-const date = (document.getElementById("date").innerHTML = today.toLocaleString(
+const date = (document.getElementById("date").innerHTML = todaysDate.toLocaleString(
   "en-US",
   options
 ));
-// const day = today.toLocaleString('en-US', options)
-// const date = document.getElementById('date').innerHTML = day;
 
-let userInput = document.getElementById("addForm"); // input value
+let userInputValue = document.getElementById("addForm");
 
-let list = document.getElementById("items"); // todo list
-
-userInput.addEventListener("submit", addItem); // add item
-list.addEventListener("click", removeItem); // remove item
-list.addEventListener("click", checkOffItem); // check-off item
+let todoList = document.getElementById("items");
 
 // add item
+userInputValue.addEventListener("submit", addItem);
+// remove item
+todoList.addEventListener("click", removeItem); 
+// check-off item
+todoList.addEventListener("click", checkOffItem);
+
 function addItem(e) {
   e.preventDefault();
+
   let newTodo = document.getElementById("addTodo").value;
   let li = document.createElement("li");
   li.id = "newTodo";
   li.appendChild(document.createTextNode(newTodo));
+
   let deleteBtn = document.createElement("span");
   deleteBtn.className = "fas fa-trash";
   li.appendChild(deleteBtn);
+
   let checkMark = document.createElement("span");
   checkMark.className = "fas fa-check";
   li.appendChild(checkMark);
-  list.appendChild(li);
+
+  todoList.appendChild(li);
 }
 
-// remove item
 function removeItem(e) {
   if (e.target.classList.contains("fa-trash")) {
     let remove = e.target.parentElement;
-    list.removeChild(remove);
+    todoList.removeChild(remove);
   }
 }
 
-// complete item
 function checkOffItem(e) {
   if (e.target.classList.contains("fa-check")) {
     e.target.parentElement.style.textDecoration = "line-through";
